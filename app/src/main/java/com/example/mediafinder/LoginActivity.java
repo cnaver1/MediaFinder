02c7f2a6970d49d0d6d2ebef2cdc6e0edf68c675
Unknown <cnaver1@lsu.edu>
2018-04-13 15:55:28 -0500

    Login Functionality
    Adds Google login funcitonality to the project.

---
+++ app/src/main/java/com/example/mediafinder/LoginActivity.java
@@ -0,0 +1,147 @@
+package com.example.mediafinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+public class LoginActivity extends AppCompatActivity {
+
+    SignInButton button;
+    private FirebaseAuth mAuth;
+    private final static int RC_SIGN_IN = 2;
+
+    GoogleApiClient mGoogleApiClient;
+    FirebaseAuth.AuthStateListener mAuthListener;
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        button = (SignInButton) findViewById(R.id.sign_in_button);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Auth.GoogleSignInApi.signOut(mGoogleApiClient);
+                signIn();
+            }
+        });
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                if(firebaseAuth.getCurrentUser() != null) {
+                    startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                }
+            }
+        };
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+            @Override
+            public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                Toast.makeText(LoginActivity.this,"Something went wrong",Toast.LENGTH_SHORT).show();
+            }
+        })
+                .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
+                .build();
+
+    }
+
+
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult task = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            // Google Sign In was successful, authenticate with Firebase
+            if(task.isSuccess()){
+                GoogleSignInAccount account = task.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            }
+            else
+            {
+                Toast.makeText(LoginActivity.this,"Auth went wrong!",Toast.LENGTH_SHORT).show();
+            }
+        }}
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG ","signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG ","signInWithCredential:failure", task.getException());
+                            Toast.makeText(LoginActivity.this,"Authentication Failed.", Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+}
--- app/src/main/java/com/example/mediafinder/MainActivity.java
+++ app/src/main/java/com/example/mediafinder/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.mediafinder;
 
 import android.content.ClipData;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -17,15 +18,43 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    Button button;
+    FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthListener;
+    GoogleApiClient mGoogleApiClient;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+        if (mAuth.getCurrentUser() == null) {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        } else {
+            Toast.makeText(MainActivity.this, "Welcome to MediaFinder "+mAuth.getCurrentUser().getDisplayName()+"!", Toast.LENGTH_SHORT).show();
+
+        }
+
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -40,6 +69,37 @@ public class MainActivity extends AppCompatActivity
 
         getSupportFragmentManager().beginTransaction().replace(R.id.content_main, new Search()).commit();
         navigationView.getMenu().getItem(0).setChecked(true);
+
+
+        if(mAuth.getCurrentUser() != null) {
+
+            String userName = mAuth.getCurrentUser().getDisplayName();
+            String userEmail = mAuth.getCurrentUser().getEmail();
+            String userPhoto = mAuth.getCurrentUser().getPhotoUrl().toString();
+
+
+
+
+            NavigationView nav = (NavigationView) findViewById(R.id.nav_view);
+            View headerView = navigationView.getHeaderView(0);
+
+            TextView navUsername = (TextView) headerView.findViewById(R.id.userName);
+            navUsername.setText(userName);
+
+            TextView navEmail = (TextView) headerView.findViewById(R.id.userEmail);
+            navEmail.setText(userEmail);
+
+            ImageView userPic = (ImageView) headerView.findViewById(R.id.userPhoto);
+
+            Glide.with(getApplicationContext()).load(userPhoto)
+                    .thumbnail(0.5f)
+                    .crossFade()
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .into(userPic);
+
+
+
+        }
     }
 
     @Override
@@ -91,6 +151,9 @@ public class MainActivity extends AppCompatActivity
                 fragment = new Settings();
                 break;
             case R.id.nav_signout:
+                mAuth.signOut();
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+
                 break;
         }
 
